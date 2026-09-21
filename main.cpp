40fe98bd50ef256f4c6efb82b07320b9fe2b1d2e
nickembrey <cne.embrey@gmail.com>
2026-09-21 19:10:33 +0000

    reference examples

--- main.cpp
+++ main.cpp
@@ -3,14 +3,60 @@
 
 using namespace std;
 
-int main() {
-    cout << "\n\nServer started...\n\n";
-    cout << "\n\nNumber of game sessions: " << GameSession::getActiveGameSessions() << "\n";
-    GameSession newGameSession("196.168.0.192", 18874);
-    cout << "\n\nNumber of game sessions: " << GameSession::getActiveGameSessions() << "\n";
-    GameSession anotherNewGameSession("196.168.0.193", 18875);
-    cout << "\n\nNumber of game sessions: " << GameSession::getActiveGameSessions() << "\n";
+class Position {
+    private:
+        int x;
+        int y;
+    public:
+        int getX() {
+            return x;
+        }
+        int getY() {
+            return y;
+        }
+        void setX(int newX) {
+            x = newX;
+        }
+        void setY(int newY) {
+            y = newY;
+        }
+        Position(int initX, int initY) {
+            x = initX;
+            y = initY;
+        }
+};
 
-    cout << "\n\nServer shutting down...\n\n";
-    return 0;
+// & means "by reference"
+int addFive(int& number) {
+    number += 5;
+    return number;
+}
+
+void addFiveToArray(int numArray[], int size) {
+    for(int i = 0; i < size; i++) {
+        numArray[i] += 5;
+    }
+}
+
+void addFiveToPosition(Position& pos) {
+    pos.setX(pos.getX() + 5);
+    pos.setY(pos.getY() + 5);
+}
+
+int main() {
+    int x = 0;
+    addFive(x); // pass by value
+    cout << "\n\nx is equal to: " << x << endl;
+    int xArray[5] = {0, 3, 7, 9, 11};
+    addFiveToArray(xArray, 5); // pass by reference
+    for(int i = 0; i < 5; i++) {
+        cout << endl << xArray[i];
+    }
+    cout << endl << endl;
+    Position myNewPosition(3,7);
+    cout << "The position's x is: " << myNewPosition.getX() << endl;
+    cout << "The position's y is: " << myNewPosition.getY() << endl;
+    addFiveToPosition(myNewPosition);
+    cout << "The position's new x is: " << myNewPosition.getX() << endl;
+    cout << "The position's new y is: " << myNewPosition.getY() << endl;
 }
